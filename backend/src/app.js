11c7e7f6ef0dc0232cbd4cf9ec5621afdacb538a
Shubham_Projects <shubham.hamirwasia03@gmail.com>
2026-07-05 21:22:04 +0530

    fixed cookies

--- backend/src/app.js
+++ backend/src/app.js
@@ -34,6 +34,12 @@ app.use(
         secret: process.env.SESSION_SECRET,
         resave: false,
         saveUninitialized: false,
+        
+        cookie: {
+        secure: process.env.NODE_ENV === "production",
+        httpOnly: true,
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    },
     })
 );
 
