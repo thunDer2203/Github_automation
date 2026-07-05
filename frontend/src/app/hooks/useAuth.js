f04494e92aa5755ff67186b2fde4cfbc2b3a400c
Shubham_Projects <shubham.hamirwasia03@gmail.com>
2026-07-05 21:36:27 +0530

    fixed useAuth hook

--- frontend/src/app/hooks/useAuth.js
+++ frontend/src/app/hooks/useAuth.js
@@ -3,14 +3,13 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
-const API = "http://localhost:5000";
 
 export default function useAuth() {
     const router = useRouter();
 
     useEffect(() => {
         async function checkAuth() {
-            const res = await fetch(`${API}/auth/me`, {
+            const res = await fetch(`${NEXT_PUBLIC_BACKEND_URL}/auth/me`, {
                 credentials: "include",
             });
 
